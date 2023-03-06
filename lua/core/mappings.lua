823ea22cc41aeda368fb80a5f155935d7818d8e6
PwnWriter <hey@pwnwriter.xyz>
2023-03-06 14:43:03 +0545

    add: keybinds for window splitting

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -16,22 +16,24 @@ map('i', '<C-l>', '<Right>')
 map('n', '<C-s>', ':w <CR>')
 map('n', '<C-q>', ':bd!<CR>')
 
-
 -- Bufferline 
 map('n', '<TAB>', ':BufferLineCycleNext<CR>')
 map('n', '<S-TAB>', ':BufferLineCyclePrev<CR>')
 
-
 -- Nvim tree
 map('n', '<leader>a', ':NvimTreeToggle<CR>')
-
 
 -- Telescope
 map('n', '<leader>F', ':Telescope find_files <CR>')
 map('n', '<leader>fl', ':Telescope live_grep <CR>')
 map('n', '<leader>th', ':Telescope colorscheme  <CR>')
 
-
-
-
+-- Window splitting
+map('n', '<leader>v', ':vsplit<CR>')
+map('n', '<leader>h', ':split<CR>')
+map('n', '<C-w>h', '<C-w>h')
+map('n', '<C-w>j', '<C-w>j')
+map('n', '<C-w>k', '<C-w>k')
+map('n', '<C-w>l', '<C-w>l')
+map('n', '<leader>x', '<C-w>c')
 
