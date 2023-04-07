0f97c4be64f5229d89e07097cc1b8806e319ac3b
Nabeen Tiwaree <hey@pwnwriter.xyz>
2023-04-07 23:58:23 +0545

    add: bash to tresitter list

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "rust", "go", "c", "lua", "toml" },
+  ensure_installed = { "rust", "go", "c", "lua", "toml", "bash" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
