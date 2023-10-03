d184f713f23514b6ad71ce44ccd27877176563da
pwnwriter <hey@pwnwriter.xyz>
2023-10-03 20:54:59 +0545

    chore(keys,mini): use mini-bufremove algorithm to remove current buffers

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,4 @@
-local binder = require("config.binder")
+local binder = require("config.binder").new()
 
 local spec = {
   "echasnovski/mini.nvim",
@@ -15,6 +15,8 @@ function spec:config()
   local surround = require("mini.surround")
   local comment = require("mini.comment")
   local files = require("mini.files")
+  local hipatterns = require("mini.hipatterns")
+  local bufremove = require("mini.bufremove")
 
   -- Configure pairs
   pairs.setup({
@@ -82,14 +84,40 @@ function spec:config()
     textobject = "gc",
   })
 
-  local binder = binder.new():with_modes({ "n" })
-
+  -- Configure files
+  binder:with_modes({ "n" })
+  binder:with_desc("Toggle mini files")
   files.setup({
     binder:bind("<leader>e", function()
       local success = files.close()
       if not success then
         files.open()
       end
+    end),
+  })
+
+  -- Configure patterns
+  hipatterns.setup({
+    highlighters = {
+      -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+      -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+      fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+      hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+      todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+      note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+      -- Highlight hex color strings (`#rrggbb`) using that color
+      hex_color = hipatterns.gen_highlighter.hex_color(),
+    },
+  })
+
+  -- Configure buffer-remover
+  binder:with_modes({ "n" })
+  binder:with_desc("Remove current buffers")
+  bufremove.setup({
+    silent = false,
+    binder:bind("<c-q>", function()
+      bufremove.delete()
     end),
   })
 end
