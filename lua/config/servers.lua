3357878cca3278a5c6a44a13805278b128d5890d
pwnwriter <hey@pwnwriter.xyz>
2023-10-04 12:09:04 +0545

    chore(server): Do not use lua-ls for formatting instead use stylua

--- lua/config/servers.lua
+++ lua/config/servers.lua
@@ -1,19 +1,21 @@
 local lspconfig = require("lspconfig")
 
 lspconfig.rust_analyzer.setup({
-  capabilities = capabilities,
-  filetypes = { "rust" },
-  cmd = { "rustup", "run", "stable", "rust-analyzer" },
+    filetypes = { "rust" },
+    cmd = { "rustup", "run", "stable", "rust-analyzer" },
 })
 
 lspconfig.lua_ls.setup({
-  capabilities = capabilities,
-  filetypes = { "lua" },
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
+    filetypes = { "lua" },
+    settings = {
+        Lua = {
+            completion = {
+                callSnippet = "Replace"
+            },
+            diagnostics = {
+                globals = { "vim" },
+            },
+            format = { enable = false },
+        },
     },
-  },
 })
