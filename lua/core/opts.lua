3609f29bc2b1f4024da12b76b9da41964eab0d28
pwnwriter <hey@pwnwriter.xyz>
2023-12-18 14:27:53 +0545

    chore(opt,lsp): use `vim.uv` instead of depcrecated `vim.loop`

--- lua/core/opts.lua
+++ lua/core/opts.lua
@@ -46,7 +46,7 @@ M.final = function()
   opt.statusline = "%#Normal#" .. statusline_ascii .. "%="
 end
 
-local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+local is_windows = vim.uv.os_uname().sysname == "Windows_NT"
 vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath "data" .. "/mason/bin"
 
 return M
