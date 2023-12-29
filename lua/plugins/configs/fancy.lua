95586a79468dc9904135e570aab1482bb8d134f1
pwnwriter <hey@pwnwriter.xyz>
2023-12-29 08:02:10 +0545

    chore(fancy): show command pallate in dropdown

--- lua/plugins/configs/fancy.lua
+++ lua/plugins/configs/fancy.lua
@@ -12,7 +12,7 @@ M.noice = {
   },
   presets = {
     bottom_search = false,
-    command_palette = true,
+    command_palette = false,
     long_message_to_split = false,
     inc_rename = false,
     lsp_doc_border = true,
