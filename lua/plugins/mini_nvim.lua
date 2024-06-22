76c27bc1f1422f1319b5fb33c5ce432fbd0e6f75
pwnwriter <hey@pwnwriter.xyz>
2024-06-22 11:05:13 +0545

    chore(plugs): sync plugins

--- lua/plugins/mini_nvim.lua
+++ lua/plugins/mini_nvim.lua
@@ -138,6 +138,7 @@ M.starter = {
       action = "lua MiniExtra.pickers.visit_paths { filter = 'todo' }",
       section = "Actions",
     },
+    { name = "Open blank file", action = ":enew", section = "Actions" },
     { name = "Find files", action = "lua MiniPick.builtin.files()", section = "Actions" },
     { name = "Recent files", action = "lua MiniExtra.pickers.oldfiles()", section = "Actions" },
     { name = "Quit", action = ":q!", section = "Actions" },
