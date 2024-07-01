e480b499b34a98ba6f6223882ee51e03ff1501e2
pwnwriter <hey@pwnwriter.xyz>
2024-07-01 12:25:30 +0545

    refactor(flake): use standard builtin method instead of flake-utils

--- flake.nix
+++ flake.nix
@@ -1,34 +1,38 @@
 {
-  description = "PwnVim:- Neovim, the less is more 👾";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    in
+    {
+      devShells = builtins.listToAttrs (map
+        (system: {
+          name = system;
+          value =
+            let
+              pkgs = import nixpkgs { inherit system; };
+            in
+            {
+              default = pkgs.mkShell {
+                packages = with pkgs; [
+                  neovim
+                  fd
+                  ripgrep
+                ];
+                shellHook = ''
+                  ln -s $(pwd) /home/$(whoami)/.config/pwnvim
+                  echo "Syncing Neovim plugins ..."
+                  nvim --headless +"Lazy! sync" +qa
+                '';
 
-          packages = with pkgs;[ 
-          neovim 
-          fd
-          ripgrep
-          ];
+                NVIM_APPNAME = "pwnvim";
 
-          shellHook = ''
-            ln -s $(pwd) /home/$(whoami)/.config/pwnvim
-	    echo "Syncing Neovim plugins ..."
-            nvim --headless +"Lazy! sync" +qa
-          '';
-
-          NVIM_APPNAME = "pwnvim";
-
-        };
-      });
+              };
+            };
+        })
+        systems);
+    };
 }
-
