b255edf57f990a27b03b116a80e6ae365db2659f
pwnwriter <hey@pwnwriter.xyz>
2024-08-11 06:52:00 +0545

    perf(entry): assure lazy being installed at entry point

--- init.lua
+++ init.lua
@@ -9,7 +9,19 @@ vim.loader.enable()
 require("core.opts").initial()
 
 local lazy_path = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazy_url = "https://github.com/folke/lazy.nvim"
 
-require("core.utils").bootstrap(lazy_path)
+if not vim.uv.fs_stat(lazy_path) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazy_url,
+    lazy_path,
+  }
+end
+
+vim.opt.rtp:prepend(lazy_path)
 
 require "plugins"
--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,17 +1,5 @@
 local M = {}
 
-M.bootstrap = function(lazy_path)
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazy_path,
-  }
-  vim.opt.rtp:prepend(lazy_path)
-end
-
 local cmds = { "nu!", "rnu!", "nonu!" }
 local current_index = 1
 
