1299c1e6bab8fc851cbf7617f195c8aeca4cab67
pwnwriter <hey@pwnwriter.xyz>
2024-09-27 10:40:24 -0400

    chore(lazy): sync neovim plugins / fixed ts lsp server

--- lua/plugins/servers.lua
+++ lua/plugins/servers.lua
@@ -39,7 +39,7 @@ lspconfig.pylsp.setup {
   },
 }
 
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
   cmd = { "typescript-language-server", "--stdio" },
   filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
   init_options = {
