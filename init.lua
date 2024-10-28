7d9a881fc9666921027d156bcf60a81a57a32965
pwnwriter <hey@pwnwriter.xyz>
2024-10-28 02:03:09 -0400

    refactor(init): moved lazy_url to same scope

--- init.lua
+++ init.lua
@@ -9,17 +9,10 @@ vim.loader.enable()
 require("opts").initial()
 
 local lazy_path = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-local lazy_url = "https://github.com/folke/lazy.nvim"
 
 if not vim.uv.fs_stat(lazy_path) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--branch=stable",
-    lazy_url,
-    lazy_path,
-  }
+  local lazy_url = "https://github.com/folke/lazy.nvim"
+  vim.fn.system { "git", "clone", "--filter=blob:none", lazy_url, "--branch=stable", lazy_path }
 end
 
 vim.opt.rtp:prepend(lazy_path)
