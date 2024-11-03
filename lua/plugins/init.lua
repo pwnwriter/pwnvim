cfa7059fe8b1aa6d2ec03e8f6c14120a010c3b75
pwnwriter <hey@pwnwriter.xyz>
2024-11-03 18:50:41 -0500

    refactor(cmp): use new preset available for keymap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -126,21 +126,15 @@ local plugins = {
         use_nvim_cmp_as_default = true,
       },
       nerd_font_variant = "mono",
-      accept = { auto_brackets = { enabled = true } },
 
-      -- trigger = { signature_help = { enabled = true } },
+      trigger = { signature_help = { enabled = true } },
 
       keymap = {
-        show = "<C-space>",
-        hide = { "<C-d>" },
-        accept = "<CR>",
-        select_prev = { "<Up>", "<C-p>" },
-        select_next = { "<Down>", "<C-n>" },
-
-        show_documentation = {},
-        hide_documentation = {},
-        scroll_documentation_up = "<C-y>",
-        scroll_documentation_down = "<C-e>",
+        ["<CR>"] = { "select_and_accept", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-y>"] = { "scroll_documentation_down" },
+        ["<C-e>"] = { "scroll_documentation_up" },
       },
 
       windows = {
