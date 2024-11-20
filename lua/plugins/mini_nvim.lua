57127da7b5af112c5421c278f0190dbfde69c73a
pwnwriter <hi@pwnwriter.me>
2024-11-20 13:50:00 -0500

    chore(starter): show intuitive icons, spaces

--- lua/plugins/mini_nvim.lua
+++ lua/plugins/mini_nvim.lua
@@ -133,15 +133,35 @@ M.starter = {
   footer = os.date(),
   items = {
     {
-      name = "Bookmarked files 󰃀",
+      name = "   Bookmarked Files",
       action = "lua MiniExtra.pickers.visit_paths { filter = 'todo' }",
-      section = "Actions ",
+      section = " Actions ", -- Nerd Font terminal icon for Actions section
     },
-    { name = "Lazy update 󰒲", action = ":Lazy update", section = "Actions " },
-    { name = "Open blank file 󰯉", action = ":enew", section = "Actions " },
-    { name = "Find files ", action = "lua MiniPick.builtin.files()", section = "Actions " },
-    { name = "Recent files ", action = "lua MiniExtra.pickers.oldfiles()", section = "Actions " },
-    { name = "Quit 󱍢", action = ":q!", section = "Actions " },
+    {
+      name = "   Lazy Update",
+      action = ":Lazy update",
+      section = " Actions ",
+    },
+    {
+      name = "   Open Blank File",
+      action = ":enew",
+      section = " Actions ",
+    },
+    {
+      name = "   Find Files",
+      action = "lua MiniPick.builtin.files()",
+      section = " Actions ",
+    },
+    {
+      name = "   Recent Files",
+      action = "lua MiniExtra.pickers.oldfiles()",
+      section = " Actions ",
+    },
+    {
+      name = "   Quit",
+      action = ":q!",
+      section = " Actions ",
+    },
   },
   content_hooks = {
     starter.gen_hook.aligning("center", "center"),
