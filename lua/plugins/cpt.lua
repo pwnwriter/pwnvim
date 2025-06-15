90741fb3d82bd0e1f1c60793041e1c25887fb124
pwnwriter <hi@pwnwriter.me>
2025-06-14 20:22:43 -0400

    chore(theme): use auto theme for cpt to toggle

--- lua/plugins/cpt.lua
+++ lua/plugins/cpt.lua
@@ -11,7 +11,6 @@ return {
     transparent_background = not vim.g.neovide,
     compile_path = vim.fn.stdpath "cache" .. "/catppuccin",
     compile = true,
-    flavour = "latte",
     integrations = {
       snacks = {
         enabled = false,
