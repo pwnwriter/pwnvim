4ce60c37e45a4a0d21b1c397267e359423e71db2
pwnwriter <hi@pwnwriter.me>
2026-09-23 22:21:20 -0400

    chore(color): use evergarden colorcheme from now

--- lua/plugins/cpt.lua
+++ lua/plugins/cpt.lua
@@ -1,34 +1,50 @@
-vim.pack.add({ { src = "https://github.com/catppuccin/nvim" } })
+vim.pack.add({ { src = "https://codeberg.org/evergarden/nvim.git", name = "evergarden" } })
 
-local ok, catppuccin = pcall(require, "catppuccin")
-if not ok then return end
-
-catppuccin.setup({
-  background = { light = "latte", dark = "mocha" },
-  float = {
-    transparent = true,
-    solid = true,
+require('evergarden').setup({
+  theme = {
+    variant = vim.o.background == "light" and "summer" or "fall",
+    accent = 'green',
   },
-  transparent_background = true,
-  compile_path = vim.fn.stdpath "cache" .. "/catppuccin",
-  compile = true,
   integrations = {
-    snacks = {
-      enabled = true,
-      indent_scope_color = "lavender",
-    },
-    treesitter = true,
     blink_cmp = true,
     mini = {
-      enabled = true,
+      enable = true,
+      animate = true,
+      clue = true,
+      completion = true,
+      cursorword = true,
+      deps = true,
+      diff = true,
+      files = true,
+      hipatterns = true,
+      icons = true,
+      indentscope = true,
+      jump = true,
+      jump2d = true,
+      map = true,
+      notify = true,
+      operators = true,
+      pick = true,
+      starters = true,
+      statusline = true,
+      surround = true,
+      tabline = true,
+      test = true,
+      trailspace = true,
+    },
+
+  },
+  editor = {
+    transparent_background = true,
+    sign = { color = 'none' },
+    float = {
+      color = 'none',
+      solid_border = true,
+    },
+    completion = {
+      color = 'none',
     },
   },
-  custom_highlights = function(C)
-    return {
-      MiniFilesTitle        = { fg = C.base, bg = C.flamingo, style = { "bold" } },
-      MiniFilesTitleFocused = { bg = C.rosewater, fg = C.base, style = { "bold", "bold" } },
-    }
-  end,
 })
 
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme "evergarden"
